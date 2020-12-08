cf1526362b775ca4e4e4b7bf1cef66f7a8b3c63f
zhang.liangliang <zhangliangliang@ouyada.com>
2020-12-08 11:02:11 +0800

    aa

--- src/main/java/com/eureka/EurekaServerApplication.java
+++ src/main/java/com/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.rsd.cloud.healthPro.config.configserver;
+package com.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ConfigServerApplication {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
